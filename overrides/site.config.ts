4f3e9717ad51570120ad87732109e2e5b43f2772
kisakimana <52074037+kisakimana@users.noreply.github.com>
2023-09-20 20:02:48 +0700

    Update site.config.ts

--- overrides/site.config.ts
+++ overrides/site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '63905c45dbd44b7da4c87bd176c0451a',
+  rootNotionPageId: '6df2983d2d8d4fe1bd95a96b404babb9',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
